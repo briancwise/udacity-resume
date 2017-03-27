6ad11ba8e2ad63f06d70bcf8187c3f47242584d7
Brian Wise <brianwisenyc@gmail.com>
2017-03-27 17:18:32 -0400

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,7 +12,7 @@ var bio = {
 		github : "briancwise",
     	twitter : "@briancwise",
     	location: "Brooklyn, NY" },
-    welcomeMessage : "Brian Wise is a web developer in training with a background in digital media",
+    welcomeMessage : "Brian Wise is a web developer in training",
 	skills : ["Javascript", " CSS", " HMTL", " jQuery"],
 	biopic : "images/fry.jpg",
 };
